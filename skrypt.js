29050c8a16b28ef1f4cb4d870f485312b398d7fc
Przemysław Gembalczyk <gembuspol@interia.pl>
2022-09-16 11:19:36 +0200

    zmiana suwaków
    Signed-off-by: Przemysław Gembalczyk <gembuspol@interia.pl>

--- skrypt.js
+++ skrypt.js
@@ -1,17 +1,23 @@
-var iloscZnakow=8;
+var iloscZn=8;
 var iloscWolnychZnakow=0;
 var iloscDuzeL=0;
 var iloscMaleL=0;
 function ustawWolne(){
-    iloscWolnychZnakow=iloscZnakow-iloscDuzeL-iloscMaleL;
-    document.getElementById("iloscDuzeL").max=iloscWolnychZnakow;
-    document.getElementById("iloscMaleL").max=iloscWolnychZnakow;
+    iloscWolnychZnakow=+iloscZn-(+iloscDuzeL+ +iloscMaleL);
+    document.getElementById("iloscDuzeL").max=+iloscWolnychZnakow+ +iloscDuzeL;
+    document.getElementById("iloscMaleL").max=+iloscWolnychZnakow+ +iloscMaleL;
 }
 function wybieranieDuzeL(){
     iloscDuzeL=document.getElementById("iloscDuzeL").value;
     document.getElementById("wybDuzeL").value=iloscDuzeL;
+    ustawWolne()
 }
 function wybieranieMaleL(){
     iloscMaleL=document.getElementById("iloscMaleL").value;
     document.getElementById("wybMaleL").value=iloscMaleL;
+    ustawWolne()
+}
+function iloscZnakow(){
+    iloscZn=document.getElementById("iloscZnakow").value;
+    ustawWolne()
 }
