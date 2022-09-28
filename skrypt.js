abe52e5a17f0a75b4d02a35ad983c91a60af30f0
Przemysław Gembalczyk <gembuspol@interia.pl>
2022-09-28 11:33:01 +0200

    losowanie znakow
    Signed-off-by: Przemysław Gembalczyk <gembuspol@interia.pl>

--- skrypt.js
+++ skrypt.js
@@ -2,19 +2,33 @@ var iloscZn=8;
 var iloscWolnychZnakow=0;
 var iloscDuzeL=0;
 var iloscMaleL=0;
+var iloscZnak=0;
+var iloscCyfr=0;
 function ustawWolne(){
-    iloscWolnychZnakow=+iloscZn-(+iloscDuzeL+ +iloscMaleL);
+    iloscWolnychZnakow=+iloscZn-(+iloscDuzeL+ +iloscMaleL+ +iloscCyfr+ +iloscZnak);
     document.getElementById("iloscDuzeL").max=+iloscWolnychZnakow+ +iloscDuzeL;
     document.getElementById("iloscMaleL").max=+iloscWolnychZnakow+ +iloscMaleL;
+    document.getElementById("iloscCyfr").max=+iloscWolnychZnakow+ +iloscCyfr;
+    document.getElementById("iloscZnak").max=+iloscWolnychZnakow+ +iloscZnak;
 }
 function wybieranieDuzeL(){
     iloscDuzeL=document.getElementById("iloscDuzeL").value;
     document.getElementById("wybDuzeL").value=iloscDuzeL;
     ustawWolne()
 }
+function wybieranieCyfr(){
+    iloscCyfr=document.getElementById("iloscCyfr").value;
+    document.getElementById("wybCyfr").value=iloscCyfr;
+    ustawWolne()
+}
 function wybieranieMaleL(){
     iloscMaleL=document.getElementById("iloscMaleL").value;
     document.getElementById("wybMaleL").value=iloscMaleL;
+    ustawWolne()
+}
+function wybieranieZnak(){
+    iloscZnak=document.getElementById("iloscZnak").value;
+    document.getElementById("wybZnak").value=iloscZnak;
     ustawWolne()
 }
 function iloscZnakow(){
@@ -25,6 +39,7 @@ function generujHaslo(){
     let haslo="";
     const alfabetMale="abcdefghijhklmnoprstuvwxyz";
     const alfabetDuze='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const alfabetZnak='!@#$%^&*<>()';
 
     //losowanie malych liter
     for(let i=0;i<iloscMaleL;i++){
@@ -33,6 +48,14 @@ function generujHaslo(){
     //losowanie duzych liter
     for(let i=0;i<iloscDuzeL;i++){
         haslo+=alfabetDuze[Math.floor(Math.random()*alfabetDuze.length)];
+    }
+    //losowanie znakow specjalnych
+    for(let i=0;i<iloscZnak;i++){
+        haslo+=alfabetZnak[Math.floor(Math.random()*alfabetZnak.length)];
+    }
+    //losowanie cyfr
+    for(let i=0;i<iloscCyfr;i++){
+        haslo+=Math.floor(Math.random()*10);
     }
    // let dane=zmianaKolejnosci(haslo);
     document.getElementById("haslo").value=zmianaKolejnosci(haslo);
