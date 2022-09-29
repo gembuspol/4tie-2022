a84e1aeb7274e5a2032390980ab83fc882e93195
Przemysław Gembalczyk <gembuspol@interia.pl>
2022-09-29 14:05:58 +0200

    gotowe
    Signed-off-by: Przemysław Gembalczyk <gembuspol@interia.pl>

--- skrypt.js
+++ skrypt.js
@@ -6,10 +6,30 @@ var iloscZnak=0;
 var iloscCyfr=0;
 function ustawWolne(){
     iloscWolnychZnakow=+iloscZn-(+iloscDuzeL+ +iloscMaleL+ +iloscCyfr+ +iloscZnak);
+
+    if(iloscWolnychZnakow<0){
+        reset();
+        alert("Funkcja idiotoodporna - za mało znaków");
+    }
     document.getElementById("iloscDuzeL").max=+iloscWolnychZnakow+ +iloscDuzeL;
     document.getElementById("iloscMaleL").max=+iloscWolnychZnakow+ +iloscMaleL;
     document.getElementById("iloscCyfr").max=+iloscWolnychZnakow+ +iloscCyfr;
     document.getElementById("iloscZnak").max=+iloscWolnychZnakow+ +iloscZnak;
+}
+function reset(){
+        iloscCyfr=0;
+        document.getElementById("iloscCyfr").value=0;
+        document.getElementById("wybCyfr").value=0;
+        iloscDuzeL=0;
+        document.getElementById("iloscDuzeL").value=0;
+        document.getElementById("wybDuzeL").value=0;
+        iloscMaleL=0;
+        document.getElementById("iloscMaleL").value=0;
+        document.getElementById("wybMaleL").value=0;
+        iloscZnak=0;
+        document.getElementById("iloscZnak").value=0;
+        document.getElementById("wybZnak").value=0;
+        iloscWolnychZnakow=iloscZn;
 }
 function wybieranieDuzeL(){
     iloscDuzeL=document.getElementById("iloscDuzeL").value;
@@ -56,6 +76,10 @@ function generujHaslo(){
     //losowanie cyfr
     for(let i=0;i<iloscCyfr;i++){
         haslo+=Math.floor(Math.random()*10);
+    }
+    //losowanie dodatkowych znaków, których nie wybrał użytkownik
+    for(let i=0;i<iloscWolnychZnakow;i++){
+        haslo+=alfabetMale.charAt(Math.floor(Math.random()*alfabetMale.length));
     }
    // let dane=zmianaKolejnosci(haslo);
     document.getElementById("haslo").value=zmianaKolejnosci(haslo);
