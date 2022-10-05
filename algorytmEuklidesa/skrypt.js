15b156c08d14ab22f42756f17915a21316808dfa
Przemysław Gembalczyk <gembuspol@interia.pl>
2022-10-05 11:21:45 +0200

    euklides
    Signed-off-by: Przemysław Gembalczyk <gembuspol@interia.pl>

---
+++ algorytmEuklidesa/skrypt.js
@@ -0,0 +1,12 @@
+function euklidesOblicz(){
+    let liczbaA=parseInt(document.getElementById("liczbaA").value);
+    let liczbaB=parseInt(document.getElementById("liczbaB").value);
+    while(liczbaA!==liczbaB){
+        if(liczbaA>liczbaB){
+            liczbaA=liczbaA-liczbaB;
+        }else{
+            liczbaB=liczbaB-liczbaA;
+        }
+    }
+    document.getElementById("euklidesWynik").value=liczbaA;
+}
