70a1d72d61c326a3acbd60331013e8e3dde50768
Przemysław Gembalczyk <gembuspol@interia.pl>
2022-10-10 12:58:04 +0200

    nww
    Signed-off-by: Przemysław Gembalczyk <gembuspol@interia.pl>

--- algorytmEuklidesa/skrypt.js
+++ algorytmEuklidesa/skrypt.js
@@ -10,3 +10,15 @@ function euklidesOblicz(){
     }
     document.getElementById("euklidesWynik").value=liczbaA;
 }
+function obliczNWW(){
+    let liczbaA=parseInt(document.getElementById("liczbaNWWA").value);
+    let liczbaB=parseInt(document.getElementById("liczbaNWWB").value);
+    let x= liczbaA*liczbaB;
+    while(liczbaB!==0){
+        let c=liczbaA%liczbaB;
+        liczbaA=liczbaB;
+        liczbaB=c;
+    }
+    let wynik=x/liczbaA;
+    document.getElementById("nwwWynik").value=wynik;
+}
