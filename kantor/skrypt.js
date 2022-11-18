0dcbf0a2c9a2a111fd44d62ba76534ea6279be98
Przemysław Gembalczyk <gembuspol@interia.pl>
2022-11-18 09:34:31 +0100

    wersja2
    Signed-off-by: Przemysław Gembalczyk <gembuspol@interia.pl>

--- kantor/skrypt.js
+++ kantor/skrypt.js
@@ -27,4 +27,31 @@ function oblicz2(){
     let waluta=document.getElementById("waluta").value;
     let tranzakcja=document.getElementById("tranzakcja").value;
     let wynik=0;
+    switch(waluta){
+        case "euro":
+            if(tranzakcja=="kupno"){
+                wynik=kwota*4.691;
+            }
+            else{
+                wynik=kwota*4.72;
+            }
+            break;
+        case "funt":
+            if(tranzakcja=="kupno"){
+                wynik=kwota*5.383;
+            }
+            else{
+                wynik=kwota*5.423;
+            }
+            break;
+        case "dolar":
+            if(tranzakcja=="kupno"){
+                wynik=kwota*4.52;
+            }
+            else{
+                wynik=kwota*4.541;
+            }
+            break;
+    }
+    document.getElementById("wynik").value=wynik;
 }
