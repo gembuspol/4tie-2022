48cd172c033e8b851255ba293134c8fdfba54081
Przemysław Gembalczyk <gembuspol@interia.pl>
2022-11-23 15:48:49 +0100

    symulacji
    Signed-off-by: Przemysław Gembalczyk <gembuspol@interia.pl>

--- mistrzostwa/skrypt.js
+++ mistrzostwa/skrypt.js
@@ -3,15 +3,19 @@ function oblicz(){
     let bramka2=document.getElementById("bramka2").value;
     let bramka3=document.getElementById("bramka3").value;
     let bramka4=document.getElementById("bramka4").value;
+    let bramka5=document.getElementById("bramka5").value;
+    let bramka6=document.getElementById("bramka6").value;
+    let bramka7=document.getElementById("bramka7").value;
+    let bramka8=document.getElementById("bramka8").value;
 
     //obliczanie punktów dla Argentyny
-    let punktyArg=punkty(bramka1,bramka2);
+    let punktyArg=punkty(bramka1,bramka2)+punkty(bramka7,bramka8);
     //obliczanie punktów dla Arabii Saudyjskiej
-    let punktyArb=punkty(bramka2,bramka1);
+    let punktyArb=punkty(bramka2,bramka1)+punkty(bramka6,bramka5);
     //obliczanie punktów dla Polski
-    let punktyPol=punkty(bramka3,bramka4);
+    let punktyPol=punkty(bramka3,bramka4)+punkty(bramka5,bramka6);
     //obliczanie punktów dla Meksyk
-    let punktyMex=punkty(bramka4,bramka3);
+    let punktyMex=punkty(bramka4,bramka3)+punkty(bramka8,bramka7);
 
     //przygotowanie wyniku
     document.getElementById("punktyArg").value=punktyArg;
